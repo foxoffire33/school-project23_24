85557001671af2e31b52b6d8225ec27817bc15fc
Reinier De La Parra <reinierdlp@gmail.com>
2022-04-13 00:54:38 +0200

    Admin dashboard exsample

--- app/Views/Layouts/Admin/header.render.php
+++ app/Views/Layouts/Admin/header.render.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tailwind CSS Simple Admin Dashboard UI Example</title>
+    <title>Page title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-tailwind.png">
     <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+    <script src="js/main.js"></script>
 </head>
+<body class="antialiased bg-body text-body font-body">
+<div class="">
